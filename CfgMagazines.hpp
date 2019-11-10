3ffd0808b8ca0ee6194a98a5ca6eaf82d91f1f55
Asterix1987 <39263191+Asterix1987@users.noreply.github.com>
2019-11-10 16:35:04 +0200

    Add files via upload

---
+++ CfgMagazines.hpp
@@ -0,0 +1,39 @@
+class CfgMagazines {
+    class DemoCharge_Remote_Mag;
+    class IEDUrbanSmall_Remote_Mag {
+        ot_craftable = 1;
+        ot_craftQuantity = 1;
+        ot_craftRecipe = "[[""HandGrenade"",4],[""OT_Steel"",1]]";
+    };
+    class IEDLandSmall_Remote_Mag {
+        ot_craftable = 1;
+        ot_craftQuantity = 1;
+        ot_craftRecipe = "[[""HandGrenade"",4],[""OT_Steel"",1]]";
+    };
+    class IEDUrbanBig_Remote_Mag {
+        ot_craftable = 1;
+        ot_craftQuantity = 1;
+        ot_craftRecipe = "[[""Titan_AT"",2],[""ACE_Cellphone"",1]]";
+    };
+    class IEDLandBig_Remote_Mag {
+        ot_craftable = 1;
+        ot_craftQuantity = 1;
+        ot_craftRecipe = "[[""Titan_AA"",2],[""ACE_Cellphone"",1]]";
+    };
+	class ACE_FlareTripMine_Mag {
+        ot_craftable = 1;
+        ot_craftQuantity = 2;
+        ot_craftRecipe = "[[""FlareWhite_F"",2],[""ACE_rope12"",1]]";
+    };
+	class APERSTripMine_Wire_Mag {
+        ot_craftable = 1;
+        ot_craftQuantity = 2;
+        ot_craftRecipe = "[[""HandGrenade"",2],[""ACE_rope12"",1]]";
+    };
+	class APERSBoundingMine_Range_Mag {
+        ot_craftable = 1;
+        ot_craftQuantity = 1;
+        ot_craftRecipe = "[[""HandGrenade"",2],[""ACE_SpraypaintBlack"",1]]";
+    };
+	
+};
